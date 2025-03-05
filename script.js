24fd080be0d16823db9d415f15c7772ad4da4d7b
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-06 02:28:02 +0530

    Add hover effect to grid divs
    Grid divs will change color upon the mouse passing over them, like a
    pixelated version of a pen writing on paper. Also, code has been put
    into functions.

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const container = document.querySelector(".container");
+function divGridCreator() {
+    const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) {
-    const secondaryContainer = document.createElement("div");
-    secondaryContainer.classList.toggle("secondary-container")
-
-    for(let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.classList.toggle("div-class");
-
-        secondaryContainer.appendChild(div);
+    for(let i = 0; i < 16; i++) {
+        const secondaryContainer = document.createElement("div");
+        secondaryContainer.classList.toggle("secondary-container")
+    
+        for(let j = 0; j < 16; j++) {
+            const div = document.createElement("div");
+            div.classList.toggle("div-class");
+    
+            secondaryContainer.appendChild(div);
+        }
+    
+        container.appendChild(secondaryContainer);
     }
-
-    container.appendChild(secondaryContainer);
 }
+
+divGridCreator();
+
+function changeColorOnHover(event) {
+    const target = event.target;
+    if(target.classList[0] === "div-class") {
+        target.classList.add("div-trail");
+    }
+}
+
+const doc = document;
+doc.addEventListener("mouseover", changeColorOnHover);
