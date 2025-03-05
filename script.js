a8b3340b56b0fd94b7c64b8f5f2f48fbe52e7ad7
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-06 00:32:38 +0530

    Create a div using JS and append it to container
    Added html boilerplate, and linked css and js files. Also added some
    declarations to a css rule for the div.

---
+++ script.js
@@ -0,0 +1,7 @@
+const divRefArray = [];
+
+const div = document.createElement("div");
+div.classList.toggle("div-class");
+
+const containerDiv = document.querySelector(".container");
+containerDiv.appendChild(div);
