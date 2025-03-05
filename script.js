8143f314e313aa17cee300588811522832dc701d
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-06 01:23:29 +0530

    Display a 16x16 grid of square divs on the webpage

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const divRefArray = [];
+const container = document.querySelector(".container");
 
-const div = document.createElement("div");
-div.classList.toggle("div-class");
+for(let i = 0; i < 16; i++) {
+    const secondaryContainer = document.createElement("div");
+    secondaryContainer.classList.toggle("secondary-container")
 
-const containerDiv = document.querySelector(".container");
-containerDiv.appendChild(div);
+    for(let j = 0; j < 16; j++) {
+        const div = document.createElement("div");
+        div.classList.toggle("div-class");
+
+        secondaryContainer.appendChild(div);
+    }
+
+    container.appendChild(secondaryContainer);
+}
